9045961cabf425d6407c59a8b658d138a8e43d7a
developer <developer@localhost>
2024-09-19 09:15:20 -0500

    Test and Lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node, // Include Node.js globals (e.g., `require`)
+        ...globals.jest  // Include Jest globals (e.g., `test`, `expect`)
+      }
+    }
+  },
   pluginJs.configs.recommended,
 ];
